a072f1cc17fea3369e030af849321ffadb7df74b
JunoJunho <wnsgh611@gmail.com>
2016-05-11 21:52:20 +0900

    Github - API test

--- AutoDo/settings.py
+++ AutoDo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AutoDoApp'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,5 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR, 'AutoDoApp/templates'),
 )
--- AutoDoApp/views.py
+++ AutoDoApp/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def index(request):
+    template = loader.get_template('AutoDoApp/index.html')
+
+    context = {
+        'access_token': "This is token",
+        'client_id': "17e7ac4cf6fc3a11f04f",
+    }
+    return HttpResponse(template.render(context=context, request=request))
+
+
+def oauth_callback(request):
+    code = ""
+    code = request.GET['code']
+    res = post_json(code)
+    template = loader.get_template('AutoDoApp/index.html')
+    context = {
+        'access_token': res,
+        'client_id': "17e7ac4cf6fc3a11f04f"
+    }
+    return HttpResponse(template.render(context=context, request=request))
+
+
+def post_json(code):
+    import json
+    import urllib.request
+
+    new_conditions = {"client_id": '17e7ac4cf6fc3a11f04f',
+                      "client_secret": '51fbf1b3ef08a04ee48680fdac6664cb08aa0071',
+                      "code": code}
+    params = json.dumps(new_conditions).encode('utf8')
+    git_api_url = "https://github.com/login/oauth/access_token"
+    req = urllib.request.Request(git_api_url, data=params,
+                                 headers={'content-type': 'application/json'})
+    response = urllib.request.urlopen(req)
+    return response
