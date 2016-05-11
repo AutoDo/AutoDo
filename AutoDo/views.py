fa08d76ba962be2515e5003623d82ebfea6e9fd6
Jusang <3kd1000@gmail.com>
2016-05-11 21:53:53 +0900

    For pull other branch

---
+++ AutoDo/views.py
@@ -0,0 +1,18 @@
+__author__ = 'user'
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django import forms
+
+def index(request):
+    return render_to_response('index.html', {}, RequestContext(request))
+
+
+def token(request):
+    token = request.GET['access_token']
+    return render_to_response('index.html', {}, RequestContext(request))
+
+def register(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
