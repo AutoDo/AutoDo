3cb7e8cf15cd431db48bf93183ab1e916b895e30
JunoJunho <wnsgh611@gmail.com>
2016-05-16 22:59:18 +0900

    Parser Implementation #10  - Source code parsing : class, method name parsing is done.  - Doing: instance name parsing

--- AutoDoApp/parser/Parser.py
+++ AutoDoApp/parser/Parser.py
@@ -14,6 +14,7 @@ class Parser(ParserCommunicator):
         self.class_dict = {}  # key: full path file_name, value: a list containing class names
         self.method_dict = {}  # key: class name, value: a list containing method names defined in the class
         self.instance_dict = {}  # key: class name, value: a list containing instance names inside the class
+        self.file_list = []
 
     def task_request(self, project_id, user_id):
         raise NotImplementedError("Implement this method!")
@@ -66,17 +67,19 @@ class Parser(ParserCommunicator):
     def __traverse_directories(self):
         # To traverse each source file, searching directories
         for key in self.dir_dict:
-            print("Directory found: " + key)
             for item in self.dir_dict[key]:
                 if key.endswith("/"):
                     path = key + item
                 else:
                     path = key + "/" + item
-                self.__traverse_source_file(path=path)
+                    self.file_list.append(path)
+        for path in self.file_list:
+            self.__traverse_source_file_path1(path=path)
+        for path in self.file_list:
+            self.__traverse_source_file_path2(path=path)
 
-    def __traverse_source_file(self, path):
+    def __traverse_source_file_path1(self, path):
         # traverse each python file
-        print("Full path: " + path)
         f = codecs.open(path, mode='r', encoding='utf-8')
         class_list = []
         lines = f.readlines()
@@ -89,18 +92,19 @@ class Parser(ParserCommunicator):
                 tokens = line.split("class")
                 cls_name = tokens[1].strip()
                 cls_name = cls_name.replace(":", "")
-                print("\tFound class: " + cls_name)
                 class_list.append(cls_name)
         if len(class_list) > 0:
             self.class_dict[path] = class_list
 
-        # Path2: traverse method name and instance variable
-        instance_list = []
+        # Path2: traverse method name
         method_list = []
         cur_context = ""
         for line in lines:
             line = line.strip()
             if line.startswith("class"):
+                if len(method_list) > 0:
+                    self.method_dict[cur_context] = method_list
+                    method_list.clear()
                 tokens = line.split("class")
                 cur_context = tokens[1].strip()
                 cur_context = cur_context.replace(":", "")
@@ -110,7 +114,45 @@ class Parser(ParserCommunicator):
                 tokens = line.split("def")
                 method_name = tokens[1].strip()
                 method_name = method_name.replace(":", "")
-                print("\tFound method: " + method_name)
+                method_list.append(method_name)
+        if len(method_list) > 0:
+            self.method_dict[cur_context] = method_list
+
+    def __traverse_source_file_path2(self, path):
+        f = codecs.open(path, mode='r', encoding='utf-8')
+        lines = f.readlines()
+        # Path3: traverse source file to parse instance variables and its call relationships
+        # Structure: first dict : {class name : second_dict}
+        #            second dict : {instance_variable : invoked method list}
+        instance_list = []
+        invoked_method_dict = {}
+        cur_context = {}
+        for line in lines:
+            line = line.strip()
+            if line.startswith("class"):
+                tokens = line.split("class")
+                cls_name = tokens[1].strip()
+                cls_name = cls_name.replace(":", "")
+                cur_context['class'] = cls_name
+            elif line.startswith("def"):
+                tokens = line.split("def")
+                method_name = tokens[1].strip()
+                method_name = method_name.replace(":", "")
+                cur_context['method'] = method_name
+            elif "=" in line and "(" in line:  # Some instance variable is assigned
+                tokens = line.split("=")
+                right_side = tokens[1].strip().split("(")[0]
+                for key in self.class_dict:
+                    for class_name in self.class_dict[key]:
+                        revised_key = class_name.split("(")[0]
+                        if revised_key == right_side:
+                            left_side = tokens[0].strip()
+                            if 'class' not in cur_context:
+                                print("Matches: " + revised_key + " Instance: " +
+                                      left_side + " in None class of " + method_name)
+                            else:
+                                print("Matches: " + revised_key + " Instance: " +
+                                      left_side + " in " + cur_context['class'] + " of " + method_name)
 
     def prev_parse_project(self):
         raise NotImplementedError("Implement this method!")
