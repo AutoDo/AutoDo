61d40feba4dd428cd0abf7f3925b9fc9061b26ee
Jusang <3kd1000@gmail.com>
2016-06-02 23:41:32 +0900

    conflict resolve

--- AutoDoApp/models.py
+++ AutoDoApp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 
-
 class GithubInformation(models.Model):
 
     email = models.CharField(max_length=200, default="none", primary_key=True)
